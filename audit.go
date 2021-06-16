96ac60406259687eba63d415da4f2907f8546786
Samuel Melrose <sam.melrose@a1comms.com>
2021-06-16 11:48:28 +0100

    Device ID from IAP

--- audit.go
+++ audit.go
@@ -6,6 +6,7 @@ import (
 
 	metadata "cloud.google.com/go/compute/metadata"
 	cl "cloud.google.com/go/logging"
+	viap "github.com/a1comms/go-middleware-validate-iap"
 )
 
 var logger *cl.Logger
@@ -29,15 +30,19 @@ func init() {
 type AuditEvent struct {
 	EventType      string      `json:"event_type"`
 	ActingIdentity string      `json:"acting_identity"`
+	DeviceID       string      `json:"device_id,omitempty"`
 	Context        interface{} `json:"context"`
 }
 
 func LogEvent(ctx context.Context, eventType, actingIdentity string, context interface{}) error {
+	claim, _ := viap.GetGoogleClaimFromContext(ctx)
+
 	err := logger.LogSync(ctx, cl.Entry{
 		Severity: cl.Info,
 		Payload: &AuditEvent{
 			EventType:      eventType,
 			ActingIdentity: actingIdentity,
+			DeviceID:       claim.DeviceID,
 			Context:        context,
 		},
 	})
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module bitbucket.org/a1commsltd/go-auditlog
 go 1.15
 
 require (
-	cloud.google.com/go v0.74.0
+	cloud.google.com/go v0.84.0
 	cloud.google.com/go/logging v1.1.2
+	github.com/a1comms/go-middleware-validate-iap v1.0.3
 )
